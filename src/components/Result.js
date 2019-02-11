dc47b2a9424a0043b132414d69ce6a76b7b1a5f4
Marina Eremina <eremina.jobmail@gmail.com>
2019-02-11 19:25:48 +0100

    edit Result.js

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
-const Result = () => {
+const Result = (props) => {
   return (
     <div>
-      <p className="result">Wynik</p>
+      <p>{props.message}</p>
+      { props.questionNumber &&
+      <p className="result">Wynik: {props.counterRight} z {props.questionNumber}.</p> }
     </div>
   );
 };
