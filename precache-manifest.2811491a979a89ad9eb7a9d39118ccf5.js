61fae01abed4e434e72113a4d36e787b0b0f3816
Marina Eremina <eremina.jobmail@gmail.com>
2019-02-12 13:27:25 +0100

    Updates

---
+++ precache-manifest.2811491a979a89ad9eb7a9d39118ccf5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9e43fe34903239bd307a1cee354c0dea",
+    "url": "/quiz-app-react/static/media/Napoleon.9e43fe34.jpg"
+  },
+  {
+    "revision": "ef065ba2f4ba07b4a59c02e47e0a1bd5",
+    "url": "/quiz-app-react/static/media/Engels.ef065ba2.jpg"
+  },
+  {
+    "revision": "3ee223226084085a68bf",
+    "url": "/quiz-app-react/static/js/runtime~main.3ee22322.js"
+  },
+  {
+    "revision": "012ef9b981cc31c1541c",
+    "url": "/quiz-app-react/static/js/main.012ef9b9.chunk.js"
+  },
+  {
+    "revision": "dbad804e680cf010f754",
+    "url": "/quiz-app-react/static/js/1.dbad804e.chunk.js"
+  },
+  {
+    "revision": "012ef9b981cc31c1541c",
+    "url": "/quiz-app-react/static/css/main.5b6b1fe2.chunk.css"
+  },
+  {
+    "revision": "752e90a3e1504750f2aafc565a578359",
+    "url": "/quiz-app-react/index.html"
+  }
+];
