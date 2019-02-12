e6823fe56a3efbc793ae411b7dff12c37dc3bbcc
Marina Eremina <eremina.jobmail@gmail.com>
2019-02-12 14:07:23 +0100

    Updates

---
+++ precache-manifest.105a7b70c5a09a8dd89f437e69e6a4bc.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "a896be2e511d320d3940cd68012ae725",
+    "url": "/quiz-app-react/static/media/Genghis.a896be2e.jpg"
+  },
+  {
+    "revision": "c2f510afb37caa7329c8",
+    "url": "/quiz-app-react/static/css/main.e1051747.chunk.css"
+  },
+  {
+    "revision": "e2c94d6492a84b490f80",
+    "url": "/quiz-app-react/static/js/1.e2c94d64.chunk.js"
+  },
+  {
+    "revision": "3ee223226084085a68bf",
+    "url": "/quiz-app-react/static/js/runtime~main.3ee22322.js"
+  },
+  {
+    "revision": "9e43fe34903239bd307a1cee354c0dea",
+    "url": "/quiz-app-react/static/media/Napoleon.9e43fe34.jpg"
+  },
+  {
+    "revision": "ef065ba2f4ba07b4a59c02e47e0a1bd5",
+    "url": "/quiz-app-react/static/media/Engels.ef065ba2.jpg"
+  },
+  {
+    "revision": "c2f510afb37caa7329c8",
+    "url": "/quiz-app-react/static/js/main.c2f510af.chunk.js"
+  },
+  {
+    "revision": "ecf00b93345c4975ca0f9bc6d12ff671",
+    "url": "/quiz-app-react/static/media/Zuma.ecf00b93.jpg"
+  },
+  {
+    "revision": "087c784085db03c1afb2d25501cbd03f",
+    "url": "/quiz-app-react/static/media/Charlemagne.087c7840.jpg"
+  },
+  {
+    "revision": "447806524dfd22b25fde781b85a22da9",
+    "url": "/quiz-app-react/static/media/Alexander-the-Great.44780652.jpg"
+  },
+  {
+    "revision": "b9f0d2f921f999bdfd30c70c831221bd",
+    "url": "/quiz-app-react/static/media/Thatcher.b9f0d2f9.jpg"
+  },
+  {
+    "revision": "c79041c18224516e887588c6d32e069d",
+    "url": "/quiz-app-react/static/media/Tito.c79041c1.jpg"
+  },
+  {
+    "revision": "d18bd137652e0788654e7071a8ab6ca9",
+    "url": "/quiz-app-react/index.html"
+  }
+];
