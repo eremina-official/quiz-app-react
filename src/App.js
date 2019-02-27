c6f13a70c3f5303a518f12319497ec7cf8f1b6c0
Marina Eremina <eremina.jobmail@gmail.com>
2019-02-27 20:09:48 +0100

    edit comments

--- src/App.js
+++ src/App.js
@@ -2,17 +2,16 @@ import React, { Component } from 'react';
 import './App.css';
 import Quiz from './components/Quiz';
 
-/* QuizContainer has a state 
-   Navigation sends which question is to be rendered
-   QuestionConteiner recieves state from QuizContainer and renders question
+/* Quiz Component has a state 
+   Navigation sends info about which question is to be rendered
+   Question receives props from Quiz and renders question 
+   Result receives props from Quiz and renders result
 */
-
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
         <header className="header">
           <p className="header__title">Quiz historyczny</p>
           <p className="header__credits">Żródło zdjęć: <br></br> Wikimedia Commons</p>
@@ -21,11 +20,8 @@ class App extends Component {
         <h1>Zgadnij kim jest ta postać</h1>
 
         <div className="quiz-container">
-
           <Quiz />
-
         </div>
-
       </div>
     );
   }
