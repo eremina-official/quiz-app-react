6b827a643c37f3940d09faea80bab284e877662f
Marina Eremina <eremina.jobmail@gmail.com>
2022-01-03 14:18:18 +0100

    Change component to a stateless function

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import Quiz from './components/Quiz';
 
@@ -8,23 +8,23 @@ import Quiz from './components/Quiz';
    Result receives props from Quiz and renders result
 */
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="header">
-          <p className="header__title">Quiz historyczny</p>
-          <p className="header__credits">Żródło zdjęć: <br></br> Wikimedia Commons</p>
-        </header>
+function App() {
+  return (
+    <div className="App">
+      <header className="header">
+        <p className="header__title">Quiz historyczny</p>
+        <p className="header__credits">
+          Żródło zdjęć: <br /> Wikimedia Commons
+        </p>
+      </header>
 
-        <h1>Zgadnij kim jest ta postać</h1>
+      <h1>Zgadnij kim jest ta postać</h1>
 
-        <div className="quiz-container">
-          <Quiz />
-        </div>
+      <div className="quiz-container">
+        <Quiz />
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
