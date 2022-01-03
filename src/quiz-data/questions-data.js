3875f6362172841d9eb438f1d263eac35a40e6e3
Marina Eremina <eremina.jobmail@gmail.com>
2022-01-03 18:39:55 +0100

    Fix linter errors in quiz-data.js

--- src/quiz-data/questions-data.js
+++ src/quiz-data/questions-data.js
@@ -12,107 +12,78 @@ const questionsData = [
     number: 1,
     image: NapoleonImage,
     hint: 'Wskazówka: nie lubił wyspy Świętej Heleny.',
-    variants: [ 
-                'Napoleon I', 
-                'Aleksander I Romanow', 
-                'George Washington'
-              ],
+    variants: ['Napoleon I', 'Aleksander I Romanow', 'George Washington'],
     correctVariant: 'Napoleon I',
-    description: 'Jeden z najwybitniejszych dowódców wojskowych w historii. Zwycięzca wielu bitew, rządził Francją przez ponad piętnaście lat.'
+    description:
+      'Jeden z najwybitniejszych dowódców wojskowych w historii. Zwycięzca wielu bitew, rządził Francją przez ponad piętnaście lat.',
   },
 
   {
     number: 2,
     image: EngelsImage,
     hint: 'Wskazówka: miał bardziej znanego kolegę.',
-    variants: [
-                'Otto von Bismarck',
-                'Georges Clemenceau',
-                'Friedrich Engels'
-              ],
+    variants: ['Otto von Bismarck', 'Georges Clemenceau', 'Friedrich Engels'],
     correctVariant: 'Friedrich Engels',
-    description: 'Filozof i ideolog komunistyczny. Jeden z wspołtwórców komunizmu.'
+    description:
+      'Filozof i ideolog komunistyczny. Jeden z wspołtwórców komunizmu.',
   },
 
   {
     number: 3,
     image: GenghisImage,
     hint: 'Wskazówka: zjednoczył swój naród i podbił pół Azji.',
-    variants: [ 
-                'Czyngis-chan', 
-                'Tamerlan', 
-                'Jumong'
-              ],
+    variants: ['Czyngis-chan', 'Tamerlan', 'Jumong'],
     correctVariant: 'Czyngis-chan',
-    description: 'Wielki zdobywca, twórca jednego z największych imperiów w historii.'
+    description:
+      'Wielki zdobywca, twórca jednego z największych imperiów w historii.',
   },
 
   {
     number: 4,
     image: ZumaImage,
     hint: 'Wskazówka: afrykański polityk, ma sześć żon.',
-    variants: [ 
-                'Jacob Zuma', 
-                'Jean-Bedel Bokassa', 
-                'Robert Mugabe'
-              ],
+    variants: ['Jacob Zuma', 'Jean-Bedel Bokassa', 'Robert Mugabe'],
     correctVariant: 'Jacob Zuma',
-    description: 'Były prezydent Republiki Południowej Afryki.'
+    description: 'Były prezydent Republiki Południowej Afryki.',
   },
 
   {
     number: 5,
     image: CharlemagneImage,
     hint: 'Wskazówka: załoźył imperium, które było prekursorem dzisiejszej Francji, Niemiec i Włoch.',
-    variants: [ 
-                'Fryderyk II Hohenstauf', 
-                'Karol Wielki', 
-                'Pepin Krótki'
-              ],
+    variants: ['Fryderyk II Hohenstauf', 'Karol Wielki', 'Pepin Krótki'],
     correctVariant: 'Karol Wielki',
-    description: 'Twórca Świętego Cesarstwa Rzymskiego, jeden z najwybitniejszych władców w dziejach średniowiecznej Europy.'
+    description:
+      'Twórca Świętego Cesarstwa Rzymskiego, jeden z najwybitniejszych władców w dziejach średniowiecznej Europy.',
   },
 
   {
     number: 6,
     image: AlexanderImage,
     hint: 'Wskazówka: podbił starożytną Persję i zapoczątkował epokę hellenistyczną.',
-    variants: [ 
-                'Filip II Macedoński', 
-                'Leonidas I', 
-                'Aleksander Macedoński'
-              ],
+    variants: ['Filip II Macedoński', 'Leonidas I', 'Aleksander Macedoński'],
     correctVariant: 'Aleksander Macedoński',
-    description: 'Jeden z największych wodzów i zdobywców w starożytności.'
+    description: 'Jeden z największych wodzów i zdobywców w starożytności.',
   },
 
   {
     number: 7,
     image: ThatcherImage,
     hint: 'Wskazówka: nazywano ją  „Żelazną Damą”.',
-    variants: [ 
-                'Eva Peron', 
-                'Margaret Thatcher', 
-                'Wilhelmina'
-              ],
+    variants: ['Eva Peron', 'Margaret Thatcher', 'Wilhelmina'],
     correctVariant: 'Margaret Thatcher',
-    description: 'Pierwsza kobieta premier w Wielkiej Brytanii.'
+    description: 'Pierwsza kobieta premier w Wielkiej Brytanii.',
   },
 
   {
     number: 8,
     image: TitoImage,
     hint: 'Wskazówka: był działaczem komunistycznym.',
-    variants: [ 
-                'Josip Broz Tito', 
-                'Benito Mussolini', 
-                'Konrad Adenauer'
-              ],
+    variants: ['Josip Broz Tito', 'Benito Mussolini', 'Konrad Adenauer'],
     correctVariant: 'Josip Broz Tito',
-    description: 'Przywódca antyniemieckiej partyzantki, wieloletni przewódca komunistycznej Jugosławii.'
+    description:
+      'Przywódca antyniemieckiej partyzantki, wieloletni przewódca komunistycznej Jugosławii.',
   },
-
 ];
 
 export default questionsData;
-
